99b317f6623f0136f424dca563cc1cfd3907ba3c
okamo <17266365+k-okamo@users.noreply.github.com>
2019-05-23 17:17:15 +0900

    [9cc:42adde9] Add tokenizer to allow whitespace between tokens.

--- 9cc.go
+++ 9cc.go
@@ -7,41 +7,114 @@ import (
 	"unicode"
 )
 
+const (
+	TK_NUM = iota + 1 // Number literal
+	TK_EOF            // End marker
+)
+
+// Token type
+type Token struct {
+	ty    int    // Token type
+	val   int    // Number literal
+	input string // Token string (for error reporting)
+}
+
+// Tokenized input is stored to this array.
+var tokens = make([]Token, 100)
+
 func main() {
 	if len(os.Args) != 2 {
 		fmt.Fprintf(os.Stderr, "Usage: 9ccgo <code>\n")
 		os.Exit(1)
 	}
 
+	tokenize(os.Args[1])
+
+	// Print the prologue
 	fmt.Printf(".intel_syntax noprefix\n")
 	fmt.Printf(".global main\n")
 	fmt.Printf("main:\n")
 
-	n, s := strtol(os.Args[1], 10)
-	fmt.Printf("\tmov rax, %d\n", n)
+	// Verify that the given expression starts with a number,
+	// and then emit the first `mov` instruction.
+	if tokens[0].ty != TK_NUM {
+		fail(0)
+	}
+	fmt.Printf("\tmov rax, %d\n", tokens[0].val)
 
-	for len(s) != 0 {
-		if []rune(s)[0] == '+' {
-			s = s[1:]
-			n, s = strtol(s, 10)
-			fmt.Printf("\tadd rax, %d\n", n)
+	// Emit assembly as we consume the sequence of `+ <number>`
+	// or `- <number>`
+	i := 1
+	for tokens[i].ty != TK_EOF {
+		if tokens[i].ty == '+' {
+			i++
+			if tokens[i].ty != TK_NUM {
+				fail(i)
+			}
+			fmt.Printf("\tadd rax, %d\n", tokens[i].val)
+			i++
 			continue
 		}
-		if []rune(s)[0] == '-' {
-			s = s[1:]
-			n, s = strtol(s, 10)
-			fmt.Printf("\tsub rax, %d\n", n)
+
+		if tokens[i].ty == '-' {
+			i++
+			if tokens[i].ty != TK_NUM {
+				fail(i)
+			}
+			fmt.Printf("\tsub rax, %d\n", tokens[i].val)
+			i++
 			continue
 		}
 
-		fmt.Fprintf(os.Stderr, "unexpected character: %c\n", []rune(s)[0])
-		os.Exit(1)
+		fail(i)
 	}
 
 	fmt.Printf("\tret\n")
 }
 
-func strtol(s string, b int) (int64, string) {
+func tokenize(s string) {
+	i := 0
+	for len(s) != 0 {
+		c := []rune(s)[0]
+		if unicode.IsSpace(c) {
+			s = s[1:]
+			continue
+		}
+
+		// + or -
+		if c == '+' || c == '-' {
+			tokens[i].ty = int(c)
+			tokens[i].input = string(c)
+			i++
+			s = s[1:]
+			continue
+		}
+
+		// Number
+		if unicode.IsDigit(c) {
+			tokens[i].ty = TK_NUM
+			tokens[i].input = string(c)
+			var val int
+			val, s = strtol(s, 10)
+			tokens[i].val = val
+			i++
+			continue
+		}
+
+		fmt.Fprintf(os.Stderr, "cannot tokenize: %s\n", string(c))
+		os.Exit(1)
+	}
+
+	tokens[i].ty = TK_EOF
+}
+
+// An error reporting function
+func fail(i int) {
+	fmt.Fprintf(os.Stderr, "unexpected token: %s\n", tokens[i].input)
+	os.Exit(1)
+}
+
+func strtol(s string, b int) (int, string) {
 	if !unicode.IsDigit([]rune(s)[0]) {
 		return 0, s
 	}
@@ -53,7 +126,7 @@ func strtol(s string, b int) (int64, string) {
 			break
 		}
 	}
-	n, _ := strconv.ParseInt(s[:j], b, 64)
-	return n, s[j:]
+	n, _ := strconv.ParseInt(s[:j], b, 32)
+	return int(n), s[j:]
 
 }
