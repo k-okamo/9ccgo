e40591131ef976a565d18ec73fcd845e665a3752
okamo <17266365+k-okamo@users.noreply.github.com>
2019-07-25 16:59:47 +0900

    [9cc:5ef04d2] Implement #define but not function-like macro.

--- preprocess.go
+++ preprocess.go
@@ -2,40 +2,86 @@ package main
 
 // C preprocessor
 
+var (
+	defined *Map
+)
+
+func append_p(v1, v2 *Vector) {
+	for i := 0; i < v2.len; i++ {
+		vec_push(v1, v2.data[i])
+	}
+}
+
 func preprocess(tokens *Vector) *Vector {
+	if defined == nil {
+		defined = new_map()
+	}
+
 	v := new_vec()
 
 	for i := 0; i < tokens.len; {
 		t := tokens.data[i].(*Token)
+		i++
+
+		if t.ty == TK_IDENT {
+			macro := map_get(defined, t.name)
+			if macro != nil {
+				append_p(v, macro.(*Vector))
+			} else {
+				vec_push(v, t)
+			}
+			continue
+		}
+
 		if t.ty != '#' {
-			i++
 			vec_push(v, t)
 			continue
 		}
 
-		i++
 		t = tokens.data[i].(*Token)
-		if t.ty != TK_IDENT || strcmp(t.name, "include") != 0 {
-			bad_token(t, "'include' expected")
+		i++
+		if t.ty != TK_IDENT {
+			bad_token(t, "identifier expected")
 		}
 
-		i++
-		t = tokens.data[i].(*Token)
-		if t.ty != TK_STR {
-			bad_token(t, "string expected")
+		if strcmp(t.name, "define") == 0 {
+			t = tokens.data[i].(*Token)
+			i++
+			if t.ty != TK_IDENT {
+				bad_token(t, "macro name expected")
+			}
+			name := t.name
+
+			v2 := new_vec()
+			for i < tokens.len {
+				t = tokens.data[i].(*Token)
+				i++
+				if t.ty == '\n' {
+					break
+				}
+				vec_push(v2, t)
+			}
+
+			map_put(defined, name, v2)
+			continue
 		}
 
-		path := t.str
+		if strcmp(t.name, "include") == 0 {
+			t = tokens.data[i].(*Token)
+			i++
+			if t.ty != TK_STR {
+				bad_token(t, "string expected")
+			}
 
-		i++
-		t = tokens.data[i].(*Token)
-		if t.ty != '\n' {
-			bad_token(t, "newline expected")
-		}
+			path := t.str
 
-		nv := tokenize(path, false)
-		for i := 0; i < nv.len; i++ {
-			vec_push(v, nv.data[i].(*Token))
+			t = tokens.data[i].(*Token)
+			i++
+			if t.ty != '\n' {
+				bad_token(t, "newline expected")
+			}
+			append_p(v, tokenize(path, false))
+			continue
 		}
 	}
 	return v
